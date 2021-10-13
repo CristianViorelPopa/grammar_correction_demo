6458f911478afc9f3690a487ca462fa35993065f
CristianViorelPopa <cristian.viorel.popa@gmail.com>
2021-10-13 16:37:21 +0300

    Formatting fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,4 @@
 import streamlit as st
-# import urllib.request
-# urllib.request.urlretrieve("https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz",
-#                            "jdk-17_linux-x64_bin.tar.gz")
-# import tarfile
-# tar = tarfile.open('jdk-17_linux-x64_bin.tar.gz', 'r:gz')
-# tar.extractall()
-# tar.close()
-# import os
-# app_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'jdk-17_linux-x64_bin', 'bin')
-# os.environ["PATH"] += os.pathsep + app_path
 import language_tool_python
 
 
@@ -20,7 +10,7 @@ with st.spinner(text='In progress'):
     tool = language_tool_python.LanguageTool('en-US')
 
 lt_form = st.form(key='language_tool_form')
-lt_text = lt_form.text_input('Text')
+lt_text = lt_form.text_input('Exter your text here:')
 lt_submit = lt_form.form_submit_button('Find mistakes')
 
 if lt_submit:
@@ -30,9 +20,9 @@ if lt_submit:
 
         st.success('There were ' + str(len(lt_matches)) + ' mistakes found in the text:')
         for idx, match in enumerate(lt_matches):
-            st.write(str(idx) + '. ' + match.ruleIssueType.upper() + ': "' + match.message + '"')
+            st.write(str(idx + 1) + '. __' + match.ruleIssueType.upper() + '__: "' + match.message + '"')
 
-        st.write('The corrected text is: "' + lt_corrected_text + '"')
+        st.write('The corrected text is: __"' + lt_corrected_text + '"__')
 
         st.write('The raw output from LanguageTool:')
         st.write(lt_matches)
